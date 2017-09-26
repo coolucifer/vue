02de45b4e5a283c51cdf77f4e8c2a06988f526bb
coolucifer <coolucifer@gmail.com>
2017-09-26 11:59:52 +0800

    commit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+var name = new Vue({
+    el: '#name', //element
+    data: {
+        name: null,
+        age: null,
+        sex: null
+    }
+})
+
+var vfor = new Vue({
+    el: '#vfor',
+    data: {
+        // foodList:['葱','姜','蒜'],
+        foodList: [{
+                name: '葱',
+                price: 10,
+                discount: 0.9,
+            },
+            {
+                name: '姜',
+                price: 15,
+                discount: 0.8,
+            },
+            {
+                name: '蒜',
+                price: 15,
+            },
+        ]
+    }
+})
+
+var vbind = new Vue({
+    el: '#vbind',
+    data: {
+        url: 'http://www.baidu.com',
+        img: 'http://img.nga.cn/attachments/mon_201709/15/-jx594Q2h-3orKlToS3m-3m.png',
+        isActive: true,
+    }
+})
+
+var von = new Vue({
+    el: '#von',
+    methods: {
+        onClick: function () {
+            alert('666');
+        },
+        onEnter: function () {
+            console.log('enter');
+        },
+        onOut: function () {
+            console.log('out');
+        },
+        onSubmit: function () {
+            // e.preventDefault();
+            console.log('submitted');
+        },
+        onEnter: function () {
+            // e.preventDefault();
+            console.log('敲回车');
+        },
+    }
+})
+
+var compute = new Vue({
+    el: '#compute',
+    data: {
+        math: 80,
+        english: 100,
+        physics: 70
+    },
+    computed: {
+        sum: function () {
+            // 如果有一栏为空则另两栏会变成字符串
+            // return this.math + this.english + this.physics;
+
+            return parseFloat(this.math) + parseFloat(this.english) + parseFloat(this.physics);
+        },
+        average: function () {
+            return Math.round(this.sum / 3);
+        }
+    }
+})
