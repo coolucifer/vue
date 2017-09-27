d0d591d4fb5d22a260d7e909dc541b62b1ffd84e
coolucifer <coolucifer@gmail.com>
2017-09-27 12:17:50 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -72,11 +72,10 @@ var compute = new Vue({
         sum: function () {
             // 如果有一栏为空则另两栏会变成字符串
             // return this.math + this.english + this.physics;
-            var result=parseFloat(this.math) + parseFloat(this.english) + parseFloat(this.physics);
-            if(!isNaN(result)){
+            var result = parseFloat(this.math) + parseFloat(this.english) + parseFloat(this.physics);
+            if (!isNaN(result)) {
                 return result;
-            }
-            else{
+            } else {
                 alert('不能为空');
             }
         },
@@ -86,18 +85,90 @@ var compute = new Vue({
     }
 });
 
-var alert_component= {
-    template:'<button @click="onClick">alert</button>',
-    methods:{
-        onClick:function(){
+var alert_component = {
+    template: '<button @click="onClick">alert</button>',
+    methods: {
+        onClick: function () {
             alert('alert');
         }
     }
 };
 
 new Vue({
-    el:'#segment',
-    components:{
-        alert:alert_component,
+    el: '#segment',
+    components: {
+        alert: alert_component,
     }
 });
+
+// ---------------父子通信------------------
+
+Vue.component('like', {
+    template: '<button :class="{liked:liked}" @click="toggle_like">赞{{like_count}}</button>',
+    data: function () {
+        return {
+            like_count: 10,
+            liked: false
+        };
+    },
+    methods: {
+        toggle_like: function () {
+            if (!this.liked) {
+                this.like_count++;
+            } else {
+                this.like_count--;
+            }
+            this.liked = !this.liked;
+        }
+    }
+})
+
+new Vue({
+    el: '#likeit',
+})
+
+Vue.component('user', {
+    template: '<a :href="\'user/\'+username">@{{username}}</a>',
+    props: ['username'],
+    methods: {}
+})
+
+new Vue({
+    el: '#users'
+})
+
+// ----------------子父通信----------------
+
+Vue.component('balance', {
+    template: `<div>
+    <show @show-balance="show_balance"></show>
+    <div v-if="show">
+    您的余额不足
+    </div>
+    </div>`,
+    methods: {
+        show_balance: function (data) {
+            this.show = true;
+            console.log('data:',data);
+        }
+    },
+    data: function () {
+        return {
+            show: false,
+        };
+    }
+});
+Vue.component('show', {
+    template: '<button @click="onClick()">显示余额</button>',
+    methods: {
+        onClick: function () {
+            this.$emit('show-balance', {
+                a: 1,
+                b: 2
+            });
+        }
+    }
+})
+new Vue({
+    el: '#balance'
+})
