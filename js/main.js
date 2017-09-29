30a79efbf9bbe07ffc355b60b2c7350fffe14690
coolucifer <coolucifer@gmail.com>
2017-09-29 10:51:27 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -209,3 +209,24 @@ Vue.component('shuandan', {
 new Vue({
     el: '#connect',
 })
+
+// ----------------filter-------------
+Vue.filter('currency', function (val, unit) {
+    val = val || 0;
+    unit = unit || '元';
+    return val + unit;
+})
+
+Vue.filter('meter', function (val, unit) {
+    val = val || 0;
+    unit = unit || 'm';
+    return (val / 1000).toFixed(2) + unit;
+})
+
+new Vue({
+    el: '#filt',
+    data: {
+        price: 10,
+        length: 10
+    }
+})
